62bf18ca98500335170b46245b2775bd9ca7f6bb
Drew <drew@cyberindie.com>
2025-11-28 15:48:45 -0700

    fix: correct Sentry organization ID for consistency

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,7 @@ export default withSentryConfig(withBundleAnalyzer(withBotId(withAxiom(nextConfi
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "dcfr-labs",
+  org: "dcyfr-labs",
 
   project: "dcyfr-labs",
 
