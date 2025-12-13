6c582094708ea8f43f8148a261a7d31ffef96c87
Drew <drew@cyberindie.com>
2025-12-12 18:34:22 -0700

    refactor(docs): simplify DevDocsPage component and remove unused imports

--- src/components/dev/index.ts
+++ src/components/dev/index.ts
@@ -1,4 +1,1 @@
 export { InteractiveDecisionTree } from "./interactive-decision-tree";
-export { DirectoryTree } from "./directory-tree";
-export { DocsSearch } from "./docs-search";
-export { DocsSidebar } from "./docs-sidebar";
